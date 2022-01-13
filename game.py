919a33e8964397e94d3f0a11645f7ab0775e9d1a
visuaalifx <kahvifetis@gmail.com>
2022-01-13 17:37:37 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,24 @@
 import pygame
 
-#Asentaa pygamen
+# Asentaa pygamen
 pygame.init()
 
-#Luo näytön pelille
+# Luo näytön pelille
 screen = pygame.display.set_mode((800, 600))
+
+# Nimi ja kuvake
+
+pygame.display.set_caption("Pyton")
+icon = pygame.image.load('ico.jpg')
+pygame.display.set_icon(icon)
+
+# Pelin Loop
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+# Taustanväri Punainen, Vihreä, Sininen (RGB)
+screen.fill ((0, 0, 0))
